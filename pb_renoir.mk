b7883fa3683713aa03990e6cb3aba8ba3a049b9c
Failedmush <anytimewicked@gmail.com>
2026-03-09 04:36:19 +0000

    renoir: Inherit GSI keys and Virtual A/B OTA
    * Add GSI keys for verification.
    * Enable Virtual A/B OTA support.

--- pb_renoir.mk
+++ pb_renoir.mk
@@ -7,6 +7,8 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Inherit from fog device
 $(call inherit-product, device/xiaomi/renoir/device.mk)
